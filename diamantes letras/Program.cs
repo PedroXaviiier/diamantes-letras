632a704a553b2175587489d1b468ec15fe915190
PedroXaviiier <phxavier.phx@gmail.com>
2022-02-17 15:01:54 -0300

    Add project files.

---
+++ diamantes letras/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace diamantes_letras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma letra: ");
+            char letra_final = Convert.ToChar(Console.ReadLine().ToLower());
+            char letra_inicial = 'a';
+            int espaco = 0;
+
+            for (letra_inicial = 'a'; letra_inicial <= letra_final; letra_inicial++)
+            {
+                Console.Write($"{new string(' ', letra_final - letra_inicial)}{letra_inicial}");
+                if (letra_inicial != 'a')
+                {
+                    Console.Write($"{new string(' ', espaco++)}{letra_inicial}");
+                }
+                espaco++;
+                Console.WriteLine();
+            }
+
+            espaco = letra_final - 'a';
+            for (letra_inicial = (char)(letra_final - 1); 'a' <= letra_inicial; letra_inicial--)
+            {
+                espaco--;
+                Console.Write($"{new string(' ', letra_final - letra_inicial)}{letra_inicial}");
+                if (letra_inicial != 'a')
+                {
+                    Console.WriteLine($"{new string(' ', espaco * 2 - 1)}{letra_inicial}");
+                }
+            }
+        }
+    }
+}
